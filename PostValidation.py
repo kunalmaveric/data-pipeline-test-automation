51f51aacfa0949dd7766f9b85237ef1b2d5257ec
VipulChaudhari23 <75496066+VipulChaudhari23@users.noreply.github.com>
2023-09-07 20:15:05 +0530

    Update PostValidation.py

--- PostValidation.py
+++ PostValidation.py
@@ -1,12 +1,7 @@
 
 class PostValidation:
     """
-       This
-       1. count - sourceData v/s targetData.
-       2. account number is 12 digit not more than that
-       3. contact number is not more than 10 digits
-       4. transactiontype should be credit or debit
-       5. dob should be yyyy-mm-dd
+       count - sourceData v/s targetData.
     """
     def countMismatch(self,spark,sourceData,targetData):
         if sourceData.count() == targetData.count():
